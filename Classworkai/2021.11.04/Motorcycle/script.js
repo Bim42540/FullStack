a6dba18e7a852bdc05abb16e8f640af93fee0b62
Bim42540 <90237833+Bim42540@users.noreply.github.com>
2021-11-04 19:58:34 +0200

    Create script.js

---
+++ Classworkai/2021.11.04/Motorcycle/script.js
@@ -0,0 +1,5 @@
+let motorcycle = new Motorcycle('Suzuki', 'Zx', 2005, 3);
+
+console.log(motorcycle.getIntroduction());
+console.log(motorcycle.getAge());
+console.log(motorcycle.getWheelsNumber());
